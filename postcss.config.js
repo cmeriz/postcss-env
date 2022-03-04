aba45504d42435b3c0aeb2dc2049513e15d69e19
cmeriz <cmerizalde27@gmail.com>
2022-03-04 18:59:35 -0500

    Adding Dev Dependencies
    Adding postcss-extend-rule & postcss-hexrgba

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,24 @@
 // PostCSS configuration
-module.exports = cfg => {
+module.exports = (cfg) => {
+    const dev = cfg.env === 'development',
+        scss = cfg.file.extname === '.scss';
 
-  const
-    dev = cfg.env === 'development',
-    scss = cfg.file.extname === '.scss';
-
-  return {
-
-    map: dev ? { inline: false } : false,
-    parser:  scss ? 'postcss-scss' : false,
-    plugins: [
-      require('postcss-advanced-variables')(),
-      require('postcss-map-get')(),
-      require('postcss-nested')(),
-      require('postcss-sort-media-queries')(),
-      require('postcss-assets')({
-        loadPaths: ['src/images/'],
-        baseUrl: dev ? 'http://localhost/postcss-demo/' : '/'
-      }),
-      require('autoprefixer')(),
-      dev ? null : require('cssnano')()
-    ]
-
-  };
-
+    return {
+        map: dev ? { inline: false } : false,
+        parser: scss ? 'postcss-scss' : false,
+        plugins: [
+            require('postcss-extend-rule')(),
+            require('postcss-advanced-variables')(),
+            require('postcss-map-get')(),
+            require('postcss-nested')(),
+            require('postcss-sort-media-queries')(),
+            require('postcss-assets')({
+                loadPaths: ['src/images/'],
+                baseUrl: dev ? 'http://localhost/postcss-demo/' : '/',
+            }),
+            require('postcss-hexrgba')(),
+            require('autoprefixer')(),
+            dev ? null : require('cssnano')(),
+        ],
+    };
 };
